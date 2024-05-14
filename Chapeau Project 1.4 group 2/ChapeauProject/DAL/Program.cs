5a47c6ed6834e507914858b69c2e2cb121d52a8d
Sukharev-ukr <64409160+Sukharev-ukr@users.noreply.github.com>
2024-05-14 13:55:49 +0200

    Add files via upload

---
+++ Chapeau Project 1.4 group 2/ChapeauProject/DAL/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Chapeau Project 1.4 group 2/ChapeauProject/Service/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Service;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

c39bdf730fd4e10db16031c31924f8e0fe4f232c
sietse <sietse.kesting@gmail.com>
2024-05-27 13:48:47 +0200

    Paymentsyste,billsplitter
    added forms and a folder for payment system

---
+++ Chapeau Project 1.4 group 2/ChapeauProject/UI/PaymentSystem/PaymentSystem.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace UI
+{
+    partial class PaymentSystem
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(117, 30, 55);
+            pictureBox1.Location = new Point(-1, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(403, 43);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(117, 30, 55);
+            pictureBox2.Location = new Point(-1, 517);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(403, 43);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // TemplateForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 223, 223);
+            ClientSize = new Size(401, 558);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "PaymentSystem";
+            Text = "PaymentSystem";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}

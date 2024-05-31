aa0deaf6d746a8a93a538ac761047b167f158f29
Sukharev-ukr <igsu2015@gmail.com>
2024-05-31 20:43:58 +0200

    Having error in terms of user is always innacurate

---
+++ Chapeau Project 1.4 group 2/ChapeauProject/Service/StaffService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+using DAL;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace Service
+{
+    public class StaffService
+    {
+        private readonly StaffDAL _staffDAL;
+
+        public StaffService()
+        {
+            _staffDAL = new StaffDAL();
+        }
+
+        
+
+        public Staff GetStaffByUsername(string username)
+        {
+            var staffList = _staffDAL.GetAllStaff();
+            foreach (var staff in staffList)
+            {
+                if (staff.Username.Equals(username, StringComparison.OrdinalIgnoreCase))
+                {
+                    return staff;
+                }
+            }
+            return null;
+        }
+
+        public bool ValidateStaffCredentials(string username, string password)
+        {
+            return _staffDAL.ValidatePassword(username, password);
+        }
+
+        
+
+        public string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}

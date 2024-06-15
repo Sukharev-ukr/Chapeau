718f4e6c5162b7a4a6be583cb80fb920752b272e
Denisa Minu <minudenisa29@gmail.com>
2024-06-16 01:19:24 +0200

    m

--- Chapeau Project 1.4 group 2/ChapeauProject/UI/Login/LoginForm.cs
+++ Chapeau Project 1.4 group 2/ChapeauProject/UI/Login/LoginForm.cs
@@ -43,19 +43,19 @@ namespace UI
         }
 
         private void getUIForStaff(Staff staff)
-        {   
-          switch (staff.Role)
+        {
+            switch (staff.Role)
             {
                 case Role.Waiter:
                     OpenUI(new TableView_Form(staff.Username));
                     break;
-                //case Role.Chef:
-                //    OpenUI(new KitchenAndBar());
-                //    break;
-                //case Role.Bartender:
-                //    OpenUI(new KitchenAndBar());
-                //    break;
-                
+                case Role.Chef:
+                    OpenUI(new OrderOverview(staff));
+                    break;
+                case Role.Bartender:
+                    OpenUI(new OrderOverview(staff));
+                    break;
+
             }
         }
 
@@ -64,15 +64,15 @@ namespace UI
         private void OpenUI(Form newForm)
         {
 
-            Program.WindowSwitcher(this, newForm);  
-/*
-            Form activeForm = ActiveForm;
-            activeForm.Hide();
+            Program.WindowSwitcher(this, newForm);
+            /*
+                        Form activeForm = ActiveForm;
+                        activeForm.Hide();
 
-            newForm.ShowDialog();
+                        newForm.ShowDialog();
 
-            
-            activeForm.Close();*/
+
+                        activeForm.Close();*/
         }
     }
 }
